f4aa29604c71f212f8577215c1c4176844fc386a
KUNZIT <vdmk1@live.com>
2025-02-13 15:33:38 +0100

    5

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -79,7 +79,9 @@ export default function Home() {
     );
 
     try {
-      const data = await verify(result, undefined, lastVerificationTime); // Pass lastVerificationTime to the server action
+      const data = await verify(result, lastVerificationTime ? parseInt(lastVerificationTime, 10) : null,
+      signal
+       );  // Pass lastVerificationTime to the server action
 
       if (data.success) {
         console.log("Successful response from backend:\n", JSON.stringify(data));
